d390e50091210529f2fd02dfee981e98401ae5d8
Franco <Franco@DESKTOP-TN4076B>
2018-10-02 22:58:37 -0300

    primer comit

--- src/banco/ATM.java
+++ src/banco/ATM.java
@@ -1,5 +1,18 @@
 package banco;
 
 public class ATM {
-
+	
+	protected ConexionDB conect;
+	
+	public  ATM (){
+		conect.getIntance();
+	}
+	
+	public boolean ingresar (int nroTarjeta, int pin) {
+		
+		
+		
+	}
+	
+	
 }
